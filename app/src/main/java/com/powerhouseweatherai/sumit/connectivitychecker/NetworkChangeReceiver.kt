c77fe1ba53569d06e0d13ec9286ebf8197f502cf
SumitRaaho <sumitkumar@raaho.in>
2023-11-15 00:40:56 +0530

    updated method for connection checker done

--- app/src/main/java/com/powerhouseweatherai/sumit/connectivitychecker/NetworkChangeReceiver.kt
+++ app/src/main/java/com/powerhouseweatherai/sumit/connectivitychecker/NetworkChangeReceiver.kt
@@ -4,24 +4,24 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import android.net.NetworkCapabilities
+
 
 class NetworkChangeReceiver(private val listener: ConnectivityChangeListener) : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         if (context != null) {
-            if (isConnected(context)) {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val network = connectivityManager.activeNetwork
+            val capabilities = connectivityManager.getNetworkCapabilities(network)
+
+            if (capabilities != null && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))) {
                 listener.onNetworkConnected()
             } else {
                 listener.onNetworkDisconnected()
             }
         }
-    }
-
-    private fun isConnected(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
-        return activeNetwork?.isConnectedOrConnecting == true
     }
 }
